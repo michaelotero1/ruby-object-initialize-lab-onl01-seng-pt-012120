fc628922804636d1d66aef35437dd1a22f648eef
michael otero <michaelotero1118@gmail.com>
2020-02-06 21:57:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,6 @@
-class Dog 
-  attr_accessor :name, :breed 
-  def initialize(name)
-    @name= name
-   end
-    
-  def initialize(breed) 
-    @breed=breed 
-  end 
-  def breed
-    puts "Mutt"
+class Dog
+  def initialize(name, breed = "Mut")
+    @name = name
+    @breed = breed
   end
 end
- 
